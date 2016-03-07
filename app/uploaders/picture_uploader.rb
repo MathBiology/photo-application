15188c8c9e4bc6b0d35deac563f1d5e3627b9f7d
MathBiology <jmartin2@ualberta.ca>
2016-03-07 16:37:11 +0000

    Ready for Heroku deployment

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,6 +13,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   else
     storage :file
   end
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -42,9 +43,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_white_list
+   def extension_white_list
      %w(jpg jpeg gif png)
-  end
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
